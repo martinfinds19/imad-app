e301b8e00ced60c3ea108d43e33a89c64199f6f0
martinfinds19 <30681879+martinfinds19@users.noreply.github.com>
2017-08-19 11:20:43 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,6 @@ element.innerHTML = 'New value';
 
 var img = document.getElementById('cage');
 
-img.onclick = function(){
-    img.style.marginRight = '100px'
+img.onclick = function() {
+    img.style.marginLeft = '100px'
 };
