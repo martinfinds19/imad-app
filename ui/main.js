cc0b3f55b6ac21f9ab51b1eea4c0db2479f120da
martinfinds19 <30681879+martinfinds19@users.noreply.github.com>
2017-08-20 00:36:45 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,5 +24,22 @@ button.onclick = function() {
     
 
 };
-
-
+   
+ var nameInput = document.getElementById('name');
+ var name = nameInput.value;
+ var submit = document.getElementById('Submit');
+  
+ submit.onclick = function() {
+       
+       
+       var names = ['name1', 'name2',  'name3', 'name4' ];
+       var list = '';
+       for(var i=0; i<names.length; i++) {
+          list == '<li>' + names[i] + '<li>';          
+       }
+       var ul = document.getElementById('nameList');
+       ul.innerHTML = List;
+ };      
+ 
+ 
+ 
