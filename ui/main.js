700f5d188e6003a3ad39c6b9ba7940058796f96d
martinfinds19 <30681879+martinfinds19@users.noreply.github.com>
2017-08-20 02:10:26 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -49,8 +49,8 @@ button.onclick = function() {
                      
     //Make request
        
- var nameInput = document.getElementById('name');
- var ephitet = nameInput.value;
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET', 'http://martinfinds19.imad.hasura-app.io/submit-name?name='  +name , true);
     request.send(null);
          
