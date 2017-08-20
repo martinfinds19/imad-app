1f14e5d5550b7583bd5ba19db67d1ad9f63a2764
martinfinds19 <30681879+martinfinds19@users.noreply.github.com>
2017-08-19 22:35:08 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,12 @@
+//Conter code
 
+var button = document.getElementbyId('counter');
+var counter = 0;
+button.onclick = function() {
+    
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+};
 
 
