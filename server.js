98427a4028114c86c73a9e78b96ba4e41f923d47
martinfinds19 <30681879+martinfinds19@users.noreply.github.com>
2017-08-19 22:19:17 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,6 +99,7 @@ app.get('/:PageNumber', function(req,res) {
     var PageNumber = req.params.PageNumber;    
      res.send(createTemplate(Pages[PageNumber]));
 });
+
 var counter = 0;
 app.get('/counter', function(req,res){
     counter = counter + 1;
