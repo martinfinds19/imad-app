e6b42ef2faa790da0c25fd717094b179c93622ae
martinfinds19 <30681879+martinfinds19@users.noreply.github.com>
2017-08-19 21:26:00 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,3 @@
 console.log('Loaded!');
 
-var img = document.getElementById('cage');
-var marginLeft = 0;
-function moveLeft () {
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft = marginLeft + 'px';
-    }
 
